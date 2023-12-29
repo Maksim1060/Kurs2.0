88dd4b2b6265363f726e3a77851b97ecf14e388c
Maksim <maks.kartashov.93@inbox.ru>
2023-12-29 14:57:59 +0200

    1

---
+++ src/main/java/kurs20/com/example/demo/entity/service/QuestionServiceImpl.java
@@ -0,0 +1,33 @@
+package kurs20.com.example.demo.entity.service;
+
+import kurs20.com.example.demo.entity.Question;
+
+import java.util.Collection;
+
+public class QuestionServiceImpl implements QuestionService {
+    @Override
+    public Question add(String question, String answer) {
+
+        return null;
+    }
+
+    @Override
+    public Question add(Question question) {
+        return null;
+    }
+
+    @Override
+    public Question remove(Question question) {
+        return null;
+    }
+
+    @Override
+    public Collection<Question> getAll() {
+        return null;
+    }
+
+    @Override
+    public Question getRandomQuestion() {
+        return null;
+    }
+}
